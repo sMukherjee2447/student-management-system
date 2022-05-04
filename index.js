8a1da25fa36218cd556a55ac025f12e1828073e9
Subham Mukherjee <smukherjee2447@gmail.com>
2022-05-04 11:54:45 +0530

    Registration and Login page done

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express')
+const req = require('express/lib/request')
 const app = express()
 const port = 3000
 
+app.set('view engine','ejs')
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('index.ejs')
+})
+
+app.get('/register', (req, res) => {
+    res.render('register.ejs')
+})
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs')
 })
 
 app.listen(port, () => {
