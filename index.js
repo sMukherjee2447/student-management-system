43beddc7361ce69cd7aaa7c993182fcddf14347f
Subham Mukherjee <smukherjee2447@gmail.com>
2022-05-10 15:51:56 +0530

    final commit

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const controller = require('./controller/controller')
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 //mongoDb connection
 var mongoose = require('mongoose')
